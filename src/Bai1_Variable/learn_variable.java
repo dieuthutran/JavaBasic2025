6af6d84eacf917d06c50f6416cda7693bf79728f
dieuthutran <tranthithudieu79@gmail.com>
2025-12-30 00:13:30 +0700

    add src folder

---
+++ src/Bai1_Variable/learn_variable.java
@@ -0,0 +1,12 @@
+package Bai1_Variable;
+
+public class learn_variable {
+    public static void main(String[] args) {
+        String nam = "Cao Diệu";
+        System.out.println("Họ tên" + nam);
+
+        int age = 1;
+        System.out.println(age);
+    }
+}
+//ctr+alt+L: fomat lại cho code dep
